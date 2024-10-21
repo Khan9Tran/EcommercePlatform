be6facb9ae4a8b2e2b8e492997930893768919f0
MinhKhanhLA62 <nguyenminhkhanhla1.6@gmail.com>
2024-10-22 05:22:18 +0700

    add search for Store

--- ecommerce-platform/src/main/java/com/hkteam/ecommerce_platform/repository/StoreRepository.java
+++ ecommerce-platform/src/main/java/com/hkteam/ecommerce_platform/repository/StoreRepository.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.hkteam.ecommerce_platform.entity.user.Store;
@@ -17,4 +18,8 @@ public interface StoreRepository extends JpaRepository<Store, String> {
 
     @NotNull
     Page<Store> findAll(@NotNull Pageable pageable);
+
+    @Query(
+            "select s from Store s where lower(s.name) like lower(concat('%', ?1, '%')) or lower(s.user.username) like lower(concat('%', ?2, '%'))")
+    Page<Store> searchAllStore(String name, String username, Pageable pageable);
 }
