f57090522df3b5b419f398e990b65deae230eacc
Khan9Tran <trankhangtctv@gmail.com>
2024-10-21 18:23:01 +0700

    add search

--- ecommerce-platform/src/main/java/com/hkteam/ecommerce_platform/controller/UserController.java
+++ ecommerce-platform/src/main/java/com/hkteam/ecommerce_platform/controller/UserController.java
@@ -120,9 +120,12 @@ public class UserController {
             @RequestParam(value = "sort", required = false, defaultValue = "") String sort,
             @RequestParam(value = "tab", required = false, defaultValue = "all") String tab,
             @RequestParam(value = "page", required = false, defaultValue = "1") String page,
-            @RequestParam(value = "size", required = false, defaultValue = "10") String size) {
+            @RequestParam(value = "size", required = false, defaultValue = "10") String size,
+            @RequestParam(value = "search", required = false, defaultValue = "") String search
+    )
+    {
         return ApiResponse.<PaginationResponse<AdminResponse>>builder()
-                .result(userService.getAllAdmins(page, size, tab, sort))
+                .result(userService.getAllAdmins(page, size, tab, sort, search))
                 .build();
     }
 }
