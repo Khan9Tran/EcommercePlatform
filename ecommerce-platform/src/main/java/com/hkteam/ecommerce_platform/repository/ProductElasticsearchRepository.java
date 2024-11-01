62db6a4dca55541bc4bb311c7bda7694bdb746ce
Khan9Tran <trankhangtctv@gmail.com>
2024-11-01 15:17:50 +0700

    fix repo

--- ecommerce-platform/src/main/java/com/hkteam/ecommerce_platform/repository/ProductElasticsearchRepository.java
+++ ecommerce-platform/src/main/java/com/hkteam/ecommerce_platform/repository/ProductElasticsearchRepository.java
@@ -1,9 +1,6 @@
 package com.hkteam.ecommerce_platform.repository;
 
 import com.hkteam.ecommerce_platform.entity.elasticsearch.ProductElasticsearch;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.List;
@@ -11,6 +8,4 @@ import java.util.List;
 public interface ProductElasticsearchRepository extends ElasticsearchRepository<ProductElasticsearch, String> {
     List<ProductElasticsearch> findByBrandId(Long brandId);
     List<ProductElasticsearch> findByCategoryId(Long categoryId);
-    Page<ProductElasticsearch> search(QueryBuilder queryBuilder, Pageable pageable);
-
 }
--- ecommerce-platform/src/main/java/com/hkteam/ecommerce_platform/service/ElasticSearchService.java
+++ ecommerce-platform/src/main/java/com/hkteam/ecommerce_platform/service/ElasticSearchService.java
@@ -1,8 +1,13 @@
 package com.hkteam.ecommerce_platform.service;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import com.hkteam.ecommerce_platform.dto.response.PaginationResponse;
 import com.hkteam.ecommerce_platform.dto.response.ProductResponse;
+import com.hkteam.ecommerce_platform.entity.elasticsearch.ProductElasticsearch;
 import com.hkteam.ecommerce_platform.repository.ProductElasticsearchRepository;
 import com.hkteam.ecommerce_platform.util.PageUtils;
 import lombok.AccessLevel;
@@ -14,9 +19,12 @@ import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.index.query.*;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 
@@ -25,90 +33,7 @@ import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
 public class ElasticSearchService {
-    ProductElasticsearchRepository productElasticsearchRepository;
 
-    public PaginationResponse<ProductResponse> getAllProducts(
-            Long categoryId,
-            Long brandId,
-            String storeId,
-            String sortBy,
-            String order,
-            String page,
-            String limit,
-            String search,
-            BigDecimal minPrice,
-            BigDecimal maxPrice,
-            int minRate,
-            Boolean isAvailable,
-            Boolean isBlocked) {
-
-        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
-
-        // Get by category, brand, store
-        if (categoryId != null) {
-            boolQuery.filter(QueryBuilders.termQuery("categoryId", categoryId));
-        }
-        if (brandId != null) {
-            boolQuery.filter(QueryBuilders.termQuery("brandId", brandId));
-        }
-        if (storeId != null) {
-            boolQuery.filter(QueryBuilders.termQuery("storeId", storeId));
-        }
-
-        // isAvailable
-        if (isAvailable != null) {
-            boolQuery.filter(QueryBuilders.termQuery("isAvailable", isAvailable));
-        }
-
-        // isBlocked
-        if (isBlocked != null) {
-            boolQuery.filter(QueryBuilders.termQuery("isBlocked", isBlocked));
-        }
-
-        // Price range
-        if (minPrice != null) {
-            boolQuery.filter(QueryBuilders.rangeQuery("salePrice").gte(minPrice));
-        }
-        if (maxPrice != null) {
-            boolQuery.filter(QueryBuilders.rangeQuery("salePrice").lte(maxPrice));
-        }
-
-        // Minimum rating
-        if (minRate > 0) {
-            boolQuery.filter(QueryBuilders.rangeQuery("rating").gte(minRate));
-        }
-
-        // Searching
-        if (search != null) {
-            boolQuery.must(QueryBuilders.multiMatchQuery(search, "name^3", "description^2", "details", "brandName", "categoryName")
-                    .fuzziness(Fuzziness.AUTO)
-                    .type(MultiMatchQueryBuilder.Type.BEST_FIELDS));
-        }
-
-        // Nested query
-        if (search != null) {
-            NestedQueryBuilder nestedQuery = QueryBuilders.nestedQuery(
-                    "productComponentValues",
-                    QueryBuilders.multiMatchQuery(search, "productComponentValues.value")
-                            .fuzziness(Fuzziness.AUTO),
-                    ScoreMode.Total
-            );
-
-            boolQuery.must(nestedQuery);
-        }
-
-
-        Sort sort;
-        if (order.equals("asc"))
-            sort = Sort.by(sortBy).ascending();
-        else
-            sort = Sort.by(sortBy).descending();
-
-        Pageable pageable = PageUtils.createPageable(page, limit, sort);
-        var result = productElasticsearchRepository.findAll(pageable);
-
-        return  null;
-    }
 
 }
 
