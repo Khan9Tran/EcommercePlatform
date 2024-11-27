9c6986eca252a2cf9e71c139da29ae80d0cd1dd6
MinhKhanhLA62 <nguyenminhkhanhla1.6@gmail.com>
2024-11-27 14:31:22 +0700

    add id for cartItem response

--- ecommerce-platform/src/main/java/com/hkteam/ecommerce_platform/dto/response/CartItemDetailResponse.java
+++ ecommerce-platform/src/main/java/com/hkteam/ecommerce_platform/dto/response/CartItemDetailResponse.java
@@ -12,6 +12,7 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CartItemDetailResponse {
+    Long id;
     String name;
     int quantity;
     BigDecimal originalPrice;
